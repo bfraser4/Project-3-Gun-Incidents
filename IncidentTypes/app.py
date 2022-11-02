80fff86cfe5f871151c56fb7b88fe33496b278eb
Brittanie <fraserbrittanie@gmail.com>
2022-11-02 10:45:11 -0700

    working changes on Flask app

--- IncidentTypes/app.py
+++ IncidentTypes/app.py
@@ -1,6 +1,6 @@
 # import numpy as np
 # from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+# from sqlalchemy.orm import Session
 # from sqlalchemy import create_engine, func
 
 from flask import Flask, request, jsonify
@@ -11,7 +11,7 @@ import sqlite3
 #################################################
 # Database Setup
 #################################################
-# engine = create_engine("sqlite:///titanic.sqlite")
+# engine = create_engine("sqlite:///incidents.sqlite")
 
 # # reflect an existing database into a new model
 # Base = automap_base()
@@ -19,7 +19,7 @@ import sqlite3
 # Base.prepare(engine, reflect=True)
 
 # # Save reference to the table
-# Passenger = Base.classes.passenger
+# Incidents = Base.classes.keys
 
 #################################################
 # Flask Setup
@@ -44,49 +44,52 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/api/v1.0/state_data<br/>"
     )
 
 
-@app.route("/api/v1.0/names")
-def names():
+@app.route("/api/v1.0/state_data")
+def data():
     # Create our session (link) from Python to the DB
     session = Session()
 
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+    """Return a list of all State data"""
+    # Query all state data
+    results = session.query(Incidents.State).all
 
     session.close()
 
     # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    # all_states = list(np.ravel(results))
 
-    return jsonify(all_names)
+        # Create a dictionary from the row data and append to a list of all_passengers
+    all_states = []
+    for state, f_year, l_year, injury, t_deaths, y_deaths, population in results:
+        state_dict = {}
+        state_dict["State"] = state
+        state_dict["First Year"] = f_year
+        state_dict["Last Year"] = l_year
+        state_dict["Injury Intent"] = injury
+        state_dict["Total Deaths (#)"] = t_deaths
+        state_dict["Avg. Yearly Deaths (#)"] = y_deaths
+        state_dict["Population"] = population
+        all_states.append(state_dict)
+
+    return jsonify(all_states)
 
 
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+# @app.route("/api/v1.0/passengers")
+# def passengers():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+#     """Return a list of State data"""
+#     # Query all passengers
+#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
 
-    session.close()
+#     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
 
-    return jsonify(all_passengers)
 
 
 if __name__ == '__main__':
