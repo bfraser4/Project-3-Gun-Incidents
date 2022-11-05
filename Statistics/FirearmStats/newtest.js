af2eb3954ae6be930ea629036704cb4deffc413d
Brittanie <fraserbrittanie@gmail.com>
2022-11-05 10:20:19 -0700

    working changes

--- Statistics/FirearmStats/newtest.js
+++ Statistics/FirearmStats/newtest.js
@@ -1,31 +1,15 @@
-function makeplot() {
-    d3.csv("/Users/brittanieocampo/Desktop/Project-3-Gun-Incidents/Statistics/firearm_homicides.csv", function(data){ processData(data) } );
-  
-  };
-  
-  function processData(allRows) {
-  
-    console.log(allRows);
-    var x = [], y = [];
-  
-    for (var i=0; i<allRows.length; i++) {
-      row = allRows[i];
-      x.push( row['Year'] );
-      y.push( row['Total_Deaths'] );
-    }
-    console.log( 'X',x, 'Y',y);
-    makePlotly( x, y);
-  }
-  
-  function makePlotly( x, y){
-    var plotDiv = document.getElementById("graph");
-    var traces = [{
-      x: x,
-      y: y
-    }];
-  
-    Plotly.newPlot('graph', traces,
-      {title: 'Plotting CSV data from AJAX call'});
-  };
-    makeplot();
+let alabama = Object.values(data.Alabama)
 
+let trace1 = {
+  x: data, 
+  y: year,
+  type: 'bar'
+}
+
+let data = [trace1];
+
+let layout = {
+  title: title
+}
+
+Plotly.newPlot('plot', data, layout)
